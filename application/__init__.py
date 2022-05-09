f62af111384a414405d217dcae363fc6e7b1ffbc
CherniK-rgb <goga.chernyavskij@bk.ru>
2022-05-09 19:45:57 +0300

    init15_postgresql_connected

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy import create_engine
+
 
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_password_Aksenof_love_db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kino.db'
+engine = create_engine("postgresql+psycopg2://root:pass@localhost/kino.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
