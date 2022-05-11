15cc2a6d04df1f744d07eb4ce7955ccec6a7c47b
CherniK-rgb <goga.chernyavskij@bk.ru>
2022-05-11 13:40:34 +0300

    Add all files

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from sqlalchemy import create_engine
-
 
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_password_Aksenof_love_db'
-engine = create_engine("postgresql+psycopg2://root:pass@localhost/kino.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kino.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
